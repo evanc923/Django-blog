612753fd284111b3059e8cd381982bafa5153864
evanc923 <evanc923@gmail.com>
2018-07-01 22:31:39 +0800

    functional blog

---
+++ mysite/urls.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from django.urls import path,re_path,include
+from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
+from django.conf import settings
+from articles import views as article_views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    re_path(r'^about/$',views.about),
+    re_path(r'^$',article_views.article_list,name="home"),
+    re_path(r'^articles/',include('articles.urls')),
+    re_path(r'^accounts/',include('accounts.urls')),
+]
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
